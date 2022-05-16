d8093b08085edcdb037c357571660949a6d98a2f
Jacob Barton <jacobdbarton3@gmail.com>
2022-05-16 17:30:33 -0400

    added header and home

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from "./components/Header";
+
+function Home() {
+  return (
+    <div className="Home">
+      <Header />
+      <h1>HI GIGI</h1>
+    </div>
+  );
+}
+
+export default Home;
