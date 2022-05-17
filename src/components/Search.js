ceef9d85b956accb7a334e2fff8efa0b25017a46
Jacob Barton <jacobdbarton3@gmail.com>
2022-05-17 18:04:46 -0400

    gigi take this

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,7 +4,7 @@ function Search() {
     return (
     <div>
         <input type='text' className='searchBar' placeholder='Search by City..'/>
-        <button></button>
+        <button>Search Icon in here...</button>
     </div>
     )
 }
