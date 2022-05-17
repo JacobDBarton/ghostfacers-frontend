f7c6d171a0245aad20a7040d0190f7adf6e6d93e
Jacob Barton <jacobdbarton3@gmail.com>
2022-05-17 17:48:15 -0400

    testing

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,14 @@
 import { Link } from "react-router-dom";
 
 function Header(props) {
-    return (
-        <nav className="nav">
-            <Link to="/">
-                <div>Home</div>  
-                <div>Sign In</div>
-                <div>Sign Up</div>
-            </Link>
-            <button>Create a spooky list</button>
-
-        </nav>
-    );
+  return (
+    <nav className="nav">
+      <Link to="/">
+        <div>Home</div>
+      </Link>
+      <button>Create a spooky list</button>
+    </nav>
+  );
 }
 
 export default Header;
--- src/components/carousel.component.js
+++ src/components/carousel.component.js
@@ -1,21 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+function CarouselComponent(props) {
+  const url = "our API";
 
-export default function CarouselComponent() {
-  return (
-    <div className="carousel-wrapper">
-      <Carousel>
-        <div>
-          <img src="https://www.history.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTU5NTM0OTYxNjc2Nzg5MzM2/halloween-haunted-house-social-getty-108309587.jpg" />
-        </div>
-        <div>
-          <img src="https://images.indianexpress.com/2019/10/Abandoned-psychiatric-hospital_759.jpg" />
-        </div>
-        <div>
-          <img src="https://whyy.org/wp-content/uploads/2019/10/2018-07-25-e-lee-philadelphia-eastern-state-penitentiary-historic-preservation-training.jpg" />
-        </div>
-      </Carousel>
-    </div>
-  );
+  const [scaryInfo, setScaryInfo] = useState([]);
+
+  useEffect(() => {
+    const getScaryInfo = async () => {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setScaryInfo(data);
+      } catch (err) {
+        console.log("hellllp");
+      }
+    };
+
+    getScaryInfo();
+  }, []);
+
+  function loaded() {
+    return (
+      <div>
+        <Carousel
+          infiniteLoop
+          autoPlay
+          stopOnHover={true}
+          showStatus={false}
+          interval={6000}
+          showThumbs={false}
+        >
+          {scaryInfo.map((image, index) => {
+            return (
+              <div key={index}>
+                <img src={image.image} alt={image.location} />
+                <h3>{image.location}</h3>
+              </div>
+            );
+          })}
+        </Carousel>
+      </div>
+    );
+  }
+
+  function loading() {
+    return <h3>Loading data...</h3>;
+  }
+
+  return <>{scaryInfo ? loaded() : loading()}</>;
 }
+
+export default CarouselComponent;
