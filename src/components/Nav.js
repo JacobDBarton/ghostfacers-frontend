bd6d2fe2bc661bf6722c131ea3bb059bf606032a
gigi-coding <brigitte.d.quintero@gmail.com>
2022-05-19 15:29:15 -0600

    styling nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import 'bootstrap'
+import { Navbar, NavItem, NavLink} from "react-bootstrap"
+
 
 const Nav = (props) => {
   return (
     <div className="nav">
+      <Navbar bg='myRed' variant="dark">
       <Link className="nav-link" to="/">
         <div>Home</div>
       </Link>
@@ -16,6 +20,9 @@ const Nav = (props) => {
       <Link className="nav-link" to="/auth/signUp">
         <div>Sign Up</div>
       </Link> */}
+      <NavItem>
+        </NavItem>
+            </Navbar>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+import "bootstrap/dist/css/bootstrap.css";
+import "./styles.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import Main from "./pages/Main";
+import Landing from "./pages/Landing";
 import Search from "./pages/Search";
 import ShowPage from "./pages/ShowPage";
-import "./styles.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -12,7 +13,7 @@ root.render(
   <Router>
     <Routes>
       <Route exact path="/" element={<App />}>
-        <Route index element={<Main />} />
+      <Route index element={<Landing />} />
         <Route path="/search" element={<Search />} />
         <Route path="/location/:id" element={<ShowPage />} />
       </Route>
